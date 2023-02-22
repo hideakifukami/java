2acf0b1d77f4972f407135e489157beca2400709
Hideaki Fukami <denys.fukami@gmail.com>
2023-02-22 08:16:27 -0300

    att

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int number1 = 12;
+        int number2 = 6;
+        number1 += 5;
+        number2 -= 2;
+
+
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number2);
+        System.out.println(number1 % number2);
     }
 }
