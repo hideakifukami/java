564575c7cd14b9d67a9becb87570ecdf445784c1
hideakifukami <denys.fukami@gmail.com>
2023-05-09 09:43:18 -0300

    att

---
+++ Aula11/ex01/ex01.java
@@ -0,0 +1,21 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class ex01 {
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int notas[] = new int[10];
+		
+		for (int i = 0; i < 10; i++) {		
+			System.out.println("Informe o número na posição " + (i + 1));
+			notas[i] = sc.nextInt();
+		}
+		
+		for (int i = 7; i < 10; i++) {
+			System.out.println(notas[i]);
+		}
+		
+	}
+}
