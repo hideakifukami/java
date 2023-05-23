f4df301f4db12bdd38bed512b05c7c799188fa80
hideakifukami <denys.fukami@gmail.com>
2023-05-23 09:26:51 -0300

    ex02

---
+++ Aula14/aula_ex01/aula_ex01.java
@@ -0,0 +1,39 @@
+package aula_ex01;
+
+import java.util.Scanner;
+
+public class aula_ex01 {
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Informe o tamanho dos vetores: ");
+		int tamanhoVetor = sc.nextInt();
+		int i = 0;
+		
+		
+		int vetorA[] = new int[tamanhoVetor];
+		int vetorB[] = new int[tamanhoVetor];
+		int vetorSoma[] = new int[tamanhoVetor];
+		int soma = 0;
+		
+		System.out.println("\n VETOR A: ");
+		for (i = 0; i < tamanhoVetor; i++) {
+			System.out.printf("Informe o valor da posição %d no vetor A: ", (i+1));
+			vetorA[i] = sc.nextInt();
+		}
+		
+		System.out.println("\n VETOR B: ");
+		for (i = 0; i < tamanhoVetor; i++) {
+			System.out.printf("Informe o valor da posição %d no vetor B: ", (i+1));
+			vetorB[i] = sc.nextInt();
+		}
+		
+		for (i = 0; i < tamanhoVetor; i++) {
+			vetorSoma[i] = vetorA[i] + vetorB[i];
+			soma += vetorSoma[i];
+		}
+		
+		System.out.println("\n A soma dos elementos no vetorSoma é: " + soma);
+					
+	}
+}
---
+++ Aula14/aula_ex02/aula_ex02.java
@@ -0,0 +1,52 @@
+package aula_ex02;
+
+import java.util.Scanner;
+
+public class aula_ex02 {
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int i = 0;
+		int soma = 0;
+		int inferiorMedia = 0;
+		int vetorTemperatura[] = new int[7];
+		
+		System.out.println("Informe a temperatura média dos dias da semana: \n");
+		
+		for (i = 0; i < 7; i++) {
+			System.out.printf("Informe a temperatura média do dia %d: ", (i+1));
+			vetorTemperatura[i] = sc.nextInt();
+		}
+		
+		int refMaior = vetorTemperatura[0];
+		int refMenor = vetorTemperatura[0];
+		
+		for (i = 0; i < 7; i++) {
+			if (refMenor > vetorTemperatura[i]) {
+				refMenor = vetorTemperatura[i];
+			}
+			
+			if (refMaior < vetorTemperatura[i]) {
+				refMaior = vetorTemperatura[i];	
+			}
+			
+			soma += vetorTemperatura[i];	
+		}
+		
+		double media = soma / 7;
+		
+		for (i = 0; i < 7; i++) {
+			if (vetorTemperatura[i] < media) {
+				inferiorMedia++;
+			}
+		}
+		
+		System.out.println("\n MENOR TEMPERATURA: " + refMenor);
+		
+		System.out.println("MAIOR TEMPERATURA: " + refMaior);
+		
+		System.out.println("TEMPERATURA MEDIA: " + media);
+		
+		System.out.println("QUANTIDADE INFERIOR A MEDIA: " + inferiorMedia);
+	}
+}
